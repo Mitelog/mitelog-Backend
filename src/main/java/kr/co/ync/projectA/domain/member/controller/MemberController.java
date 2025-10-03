5e48a0dc6b61cc178298e8fe0d1c3de4c59db245
mitae2 <kjhmen2@gmail.com>
2025-10-03 19:38:46 +0900

    test

--- src/main/java/kr/co/ync/projectA/domain/member/controller/MemberController.java
+++ src/main/java/kr/co/ync/projectA/domain/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://3.36.91.73")
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
