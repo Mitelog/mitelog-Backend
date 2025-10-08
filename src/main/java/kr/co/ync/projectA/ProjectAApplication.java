3119c498935d4071a604f5456016b84fc7d8eb84
mitae2 <kjhmen2@gmail.com>
2025-10-08 18:52:00 +0900

    test

--- src/main/java/kr/co/ync/projectA/ProjectAApplication.java
+++ src/main/java/kr/co/ync/projectA/ProjectAApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = "kr.co.ync.projectA",  exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableJpaAuditing
 public class ProjectAApplication {
 
