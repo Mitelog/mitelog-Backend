3427ec92abf35e68b4b302b41ddc6f1a1be70b0a
ksdr123 <tmdgus0880@gmail.com>
2025-10-11 21:44:20 +0900

    [Feat] Category 연동 기능 추가 (#77)

--- src/main/java/kr/co/ync/projectA/domain/category/repository/CategoryRepository.java
+++ src/main/java/kr/co/ync/projectA/domain/category/repository/CategoryRepository.java
@@ -1,7 +1,10 @@
 package kr.co.ync.projectA.domain.category.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import kr.co.ync.projectA.domain.category.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    Optional<CategoryEntity> findByName(String name);
 }
