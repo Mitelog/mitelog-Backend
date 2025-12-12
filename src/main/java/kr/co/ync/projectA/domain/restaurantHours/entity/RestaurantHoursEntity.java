84c641825463fc9a4f08146a9730c98283fb0191
ksdr123 <tmdgus0880@gmail.com>
2025-12-12 09:51:04 +0900

    [Feat] 레스토랑 영업시간 엔티티 수정 및 요일 필드를 enum으로 변경(#172)

--- src/main/java/kr/co/ync/projectA/domain/restaurantHours/entity/RestaurantHoursEntity.java
+++ src/main/java/kr/co/ync/projectA/domain/restaurantHours/entity/RestaurantHoursEntity.java
@@ -1,13 +1,14 @@
 package kr.co.ync.projectA.domain.restaurantHours.entity;
 
 import jakarta.persistence.*;
+import kr.co.ync.projectA.domain.restaurantHours.enums.DayOfWeekType;
 import lombok.*;
 import kr.co.ync.projectA.domain.restaurant.entity.RestaurantEntity;
 
 import java.time.LocalTime;
 
 @Entity
-@Table(name = "tbl_restaurantHours", uniqueConstraints = @UniqueConstraint(columnNames = {"restaurantId", "dayOfWeek"}))
+@Table(name = "tbl_restaurant_hours", uniqueConstraints = @UniqueConstraint(columnNames = {"restaurant_id", "day_of_week"}))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
@@ -20,15 +21,16 @@ public class RestaurantHoursEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id", nullable = false)
-    private RestaurantEntity restaurantId;
+    private RestaurantEntity restaurant;
 
-    @Column(nullable = false) //월~일 사이로만 선택 가능 enum 사용할까 생각 중
-    private String dayOfWeek;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "day_of_week") //월~일 사이로만 선택 가능 enum 사용할까 생각 중
+    private DayOfWeekType dayOfWeek;
 
-    @Column
+    @Column(name = "open_time")
     private LocalTime openTime;
 
-    @Column
+    @Column(name = "close_time")
     private LocalTime closeTime;
 
     @Column(nullable = false)
